c4648a5e7383f9450370ae1e45f8f37861eb6332
keigo <k5.trismegistus@gmail.com>
2020-12-09 00:14:46 +0900

    :sparkles: Add pages

--- src/popup/Popup.tsx
+++ src/popup/Popup.tsx
@@ -1,18 +1,37 @@
 import React, { useEffect, useState } from "react"
 import "./Popup.scss"
 
-const Popup = ({currentTab}) => {
+const Popup = ({currentUrl, currentTitle}) => {
+  const [bookmarksCurrentHost, setBookmarksCurrentHost] = useState([])
   useEffect(() => {
+    chrome.storage.sync.get([currentUrl.host], (result) => {
+      setBookmarksCurrentHost(result[currentUrl.host])
+    })
+
     chrome.runtime.sendMessage({ popupMounted: true })
   }, [])
 
+  const addCurrentPage = () => {
+    chrome.storage.sync.get([currentUrl.host], (result) => {
+      const currentBookmark = result[currentUrl.host] ? result[currentUrl.host] : []
+      currentBookmark.push({title: currentTitle, url: currentUrl.toString()})
+      chrome.storage.sync.set({[currentUrl.host]: currentBookmark })
+    })
+  }
+
   return <div className="popupContainer">
-    <p>
-      {currentTab.title}
-    </p>
-    <p>
-      {currentTab.url}
-    </p>
+    {bookmarksCurrentHost.map((bm) => (
+      <div>
+        <p>
+          {bm.title}
+        </p>
+        <p>
+          {bm.url}
+        </p>
+      </div>
+    ))}
+
+    <button onClick={() => addCurrentPage()}>Add current page</button>
   </div>
 }
 
--- src/popup/index.tsx
+++ src/popup/index.tsx
@@ -5,5 +5,13 @@ import Popup from './Popup'
 chrome.tabs.query({ active: true, currentWindow: true }, tabs => {
     const currentTab = tabs[0]
 
-    ReactDOM.render(<Popup currentTab={currentTab} />, document.getElementById('popup'))
+    const url = new URL(currentTab.url)
+    const title = currentTab.title
+
+    ReactDOM.render(
+        <Popup
+            currentUrl={url}
+            currentTitle={title}
+        />
+    , document.getElementById('popup'))
 })
